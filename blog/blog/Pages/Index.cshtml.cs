4b8b8a29f483a79032cfa81c7ee55dc8972d1150
miladhzz <miladhzz@gmail.com>
2026-02-27 11:09:16 +0330

    ini blog

---
+++ blog/blog/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
