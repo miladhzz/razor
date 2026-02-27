b80356818c94ac223f2f460c1adecfd8383cf8a2
miladhzz <miladhzz@gmail.com>
2026-02-27 14:17:44 +0330

    migrate db code first

---
+++ blog/blog/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace blog.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+  }
+}
